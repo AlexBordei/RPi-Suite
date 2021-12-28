6fc20c7b366afb4711f481b2526806fff41df37e
Alex Bordei <alex@whiz.ro>
2021-12-28 12:24:31 +0200

    Updates

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import Adafruit_BMP.BMP085 as BMP085
 import time
+import os
 
 
 def setup():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print('\n Barometer begins...')
 
 
@@ -12,11 +14,12 @@ def loop():
         temp = sensor.read_temperature()  # Read temperature to veriable temp
         pressure = sensor.read_pressure()  # Read pressure to veriable pressure
 
-        print('')
+        print('      Barometer')
         print('      Temperature = {0:0.2f} C'.format(temp))  # Print temperature
         print('      Pressure = {0:0.2f} Pa'.format(pressure))  # Print pressure
         time.sleep(10)
         print('')
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def destroy():
